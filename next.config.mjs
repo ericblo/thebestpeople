5bfb4a0534dbb3d01852595896df0409ba84b27f
Eric Blohm <blohm.eric@gmail.com>
2026-04-23 13:30:57 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   basePath: '/thebestpeople',
-};
-
-export default nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/thebestpeople',
+        permanent: true,
+        basePath: false, // This allows the redirect to work from the actual root
+      },
+    ]
+  },
+}
